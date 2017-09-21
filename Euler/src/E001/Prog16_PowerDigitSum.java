ffefa5f1306de3ba3fe0fbdbf4f660e90bb32f12
johnnylchang <riversnow@gmail.com>
2017-09-21 18:00:55 +0800

    Euler program

---
+++ Euler/src/E001/Prog16_PowerDigitSum.java
@@ -0,0 +1,25 @@
+package E001;
+
+import java.math.BigInteger;
+
+public class Prog16_PowerDigitSum implements EulerProg {
+	public static void main(String[] args) {
+		System.out.println(new Prog16_PowerDigitSum().run());
+	}
+	
+	private long Power(long x) {
+		BigInteger tmp = new BigInteger("2");
+		tmp = tmp.pow(1000);
+		char [] s = tmp.toString().toCharArray();
+		long sum = 0;
+		for(char c :s)
+			sum += Character.getNumericValue(c);
+		return sum;
+	}
+
+	@Override
+	public String run() {
+		return String.valueOf(Power(1000));
+	}
+
+}
