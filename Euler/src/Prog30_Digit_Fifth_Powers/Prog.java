1266b5bd93139a637661addc5c6e143ead7dc6f4
johnnylchang <riversnow@gmail.com>
2017-10-18 14:07:37 +0800

    prog 30

---
+++ Euler/src/Prog30_Digit_Fifth_Powers/Prog.java
@@ -0,0 +1,25 @@
+package Prog30_Digit_Fifth_Powers;
+
+import base.EulerProgBase;
+
+public class Prog extends EulerProgBase {
+	public static void main(String[] args) {
+		System.out.println(new Prog().run());
+	}
+
+	@Override
+	public String BruteForce() {
+		long sum = 0;
+		for (int i = 0; i < 10; ++i) {
+
+		}
+		return String.valueOf(sum);
+	}
+
+	@Override
+	public String Smart() {
+		long sum = 0;
+		return String.valueOf(sum);
+	}
+
+}
