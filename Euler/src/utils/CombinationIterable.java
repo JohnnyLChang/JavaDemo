30676fda6c6749c5f54ac3406c400e87af3fc2bc
johnnylchang <riversnow@gmail.com>
2017-10-18 14:03:36 +0800

    initial CombinationIterable

---
+++ Euler/src/utils/CombinationIterable.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CombinationIterable<T> implements Iterable<List<T>> {
+
+	@Override
+	public Iterator<List<T>> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
