84f168d69d08ad1893b8c8e80a112b29ee98aac9
LFChang <riversnow@gmail.com>
2017-10-19 15:33:49 +0800

    Prog30 - Improve solution space 1. reduce the from 1000000 to 355000

--- Euler/src/Prog30_Digit_Fifth_Powers/Prog.java
+++ Euler/src/Prog30_Digit_Fifth_Powers/Prog.java
@@ -1,3 +1,9 @@
+/* 
+ * Solution to Project Euler problem 30
+ * Copyright (c) Project Johnny. All rights reserved.
+ * 
+ */
+
 package Prog30_Digit_Fifth_Powers;
 
 import java.util.ArrayList;
@@ -11,70 +17,67 @@ public class Prog extends EulerProgBase {
 	public static void main(String[] args) {
 		System.out.println(new Prog().run());
 	}
-	
+
 	private boolean tokenCompare(String s, long v) {
 		String vstr = String.valueOf(v);
-		if(s.length() != vstr.length()) return false;
-		
-		
+		if (s.length() != vstr.length())
+			return false;
+
 		ArrayList<Character> setS = new ArrayList<Character>();
 		ArrayList<Character> setV = new ArrayList<Character>();
-		for(char c : s.toCharArray())
-		    setS.add(c);
-		for(char c : vstr.toCharArray())
-		    setV.add(c);
+		for (char c : s.toCharArray())
+			setS.add(c);
+		for (char c : vstr.toCharArray())
+			setV.add(c);
 		setS.sort((a, b) -> a.compareTo(b));
 		setV.sort((a, b) -> a.compareTo(b));
 		return setS.equals(setV);
 	}
-	
+
 	@Override
 	public String BruteForce() {
 		long sum = 0;
-		Integer[] v = IntStream.range(0, 10).boxed().toArray( Integer[]::new );
-		CombinationRepeat<Integer> a = new CombinationRepeat<Integer>(v, 5);
-        for(List<Integer> l : a) {
-        	long tmp = 0;
-        	for(Integer x : l) {
-        		tmp += (long) Math.pow(x, 5);
-        	}
-        	if(tokenCompare(a.toString(l), tmp)) {
-        		a.printPretty(l);
-        		System.out.println(tmp);
-        		sum += tmp;
-        	}
-        }
-        CombinationRepeat<Integer> b = new CombinationRepeat<Integer>(v, 6);
-        for(List<Integer> l : b) {
-        	long tmp = 0;
-        	for(Integer x : l) {
-        		tmp += (long) Math.pow(x, 5);
-        	}
-        	if(tokenCompare(b.toString(l), tmp)) {
-        		a.printPretty(l);
-        		System.out.println(tmp);
-        		sum += tmp;
-        	}
-        }
-        CombinationRepeat<Integer> c = new CombinationRepeat<Integer>(v, 4);
-        for(List<Integer> l : c) {
-        	long tmp = 0;
-        	for(Integer x : l) {
-        		tmp += (long) Math.pow(x, 5);
-        	}
-        	if(tokenCompare(a.toString(l), tmp)) {
-        		a.printPretty(l);
-        		System.out.println(tmp);
-        		sum += tmp;
-        	}
-        }
+		Integer[] v = IntStream.range(0, 10).boxed().toArray(Integer[]::new);
+
+		for (int i = 4; i <= 6; ++i) {
+			CombinationRepeat<Integer> a = new CombinationRepeat<Integer>(v, i);
+			for (List<Integer> l : a) {
+				long tmp = 0;
+				for (Integer x : l) {
+					tmp += (long) Math.pow(x, 5);
+				}
+				if (tokenCompare(a.toString(l), tmp)) {
+					a.printPretty(l);
+					System.out.println(tmp);
+					sum += tmp;
+				}
+			}
+		}
 		return String.valueOf(sum);
 	}
 
 	@Override
 	public String Smart() {
-		long sum = 0;
-		return String.valueOf(sum);
+		// As stated in the problem, 1 = 1^5 is excluded.
+		// If a number has at least n >= 7 digits, then even if every digit is 9,
+		// n * 9^5 is still less than the number (which is at least 10^n).
+		// 6 digit number. 6*95 = 354294. So 355000, reduce the solution space
+		int sum = 0;
+		for (int i = 2; i < 355000; i++) {
+			if (i == fifthPowerDigitSum(i))
+				sum += i;
+		}
+		return Integer.toString(sum);
+	}
+
+	private static int fifthPowerDigitSum(int x) {
+		int sum = 0;
+		while (x != 0) {
+			int y = x % 10;
+			sum += y * y * y * y * y;
+			x /= 10;
+		}
+		return sum;
 	}
 
 }
