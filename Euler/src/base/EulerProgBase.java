e54ec2c516f8b9097470f0b2937a6a260c531af6
LFChang <riversnow@gmail.com>
2017-10-20 13:30:58 +0800

    Using Tasks to execute solutions 1. Add Command interface for sol 2. Using Anonymous class to wrap sol func 3. Keep original function without modifying entiring project

--- Euler/src/base/EulerProgBase.java
+++ Euler/src/base/EulerProgBase.java
@@ -1,17 +1,51 @@
 package base;
 
+import java.util.ArrayList;
+
 public abstract class EulerProgBase implements EulerProg {
 	private long startTime;
 	protected String name;
+	ArrayList<Command> tasks;
+
+	public interface Command {
+		public String name();
+
+		public String execute();
+	}
+
+	public void AddTasks(Command e) {
+		tasks.add(e);
+	}
 
 	public EulerProgBase() {
+		tasks = new ArrayList<Command>();
 		this.name = this.getClass().getPackage().getName();
+		tasks.add(new Command() {
+			public String name() {
+				return "Brute Force";
+			}
+
+			public String execute() {
+				return EulerProgBase.this.BruteForce();
+			}
+		});
+		tasks.add(new Command() {
+			public String name() {
+				return "Smart";
+			}
+
+			public String execute() {
+				return EulerProgBase.this.Smart();
+			}
+		});
+
 		info();
 	}
+
 	public void info() {
 		System.out.println(name);
 	}
-	
+
 	private void start() {
 		startTime = System.nanoTime();
 	}
@@ -20,34 +54,29 @@ public abstract class EulerProgBase implements EulerProg {
 		long elapsed = (System.nanoTime() - startTime) / 1000;
 		System.out.println(elapsed + " micro seconds");
 	}
-	
+
 	private void printline() {
 		System.out.println("=================================");
 	}
-	
+
 	public String BruteForce() {
 		return "Brute Force result";
 	}
-	
+
 	public String Smart() {
 		return "Smart result";
 	}
-	
+
 	@Override
 	public String run() {
-		printline();
-		System.out.println("Brute force solution");
-		start();
-		String ret1 = BruteForce();
-		elapse();
-		System.out.println("Solutoin:"+ret1);
-		printline();
-		System.out.println("Smart solution");
-		start();
-		String ret2 = Smart();
-		elapse();
-		System.out.println("Solutoin:"+ret2);
-		printline();
+		for (Command c : tasks) {
+			printline();
+			System.out.println(c.name());
+			start();
+			String ret1 = c.execute();
+			elapse();
+			System.out.println("Solutoin:" + ret1);
+		}
 		return "";
 	}
 }
